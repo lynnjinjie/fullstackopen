cf663bb0b14eda31e6f54f6ad8fcf6d6afd81cf1
jinjie <lynnjinjie@gmail.com>
2020-06-07 11:06:06 +0800

    readme-update

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 - [x] par0
 - [x] part1
-- [] part2
+- [ ] part2
+- [ ] part3
+- [ ] part4
+- [ ] part5
+- [ ] part6
+- [ ] part7
+- [ ] part8
+- [ ] part9
---
+++ course/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// import axios from 'axios'
+
+// axios.get('http://localhost:3001/notes').then((res) => {
+//   const notes = res.data
+//   ReactDOM.render(<App notes={notes} />, document.getElementById('root'))
+// })
+ReactDOM.render(<App />, document.getElementById('root'))
