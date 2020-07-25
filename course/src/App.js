4b181245cf8a82d03e693ed4ca5dd17691310c4e
jinjie <lynnjinjie@gmail.com>
2020-07-25 19:16:14 +0800

    update

--- course/src/App.js
+++ course/src/App.js
@@ -7,32 +7,49 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
+    // console.log('effect')
     axios.get('http://localhost:3001/notes').then((res) => {
-      console.log('promise fulfilled')
+      // console.log('promise fulfilled')
       setNotes(res.data)
     })
   }, [])
-  console.log('render', notes.length, 'notes')
+  // console.log('render', notes.length, 'notes')
 
+  // 添加note
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
+      // 让服务器生成id
+      // id: notes.length + 1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios.post('http://localhost:3001/notes', noteObject).then((res) => {
+      console.log('res', res)
+      setNotes(notes.concat(res.data))
+      setNewNote('')
+    })
   }
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNote(event.target.value)
   }
+  // 显示的note
   const noteToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true)
+
+  // 切换note重要性
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find((n) => n.id === id)
+    const changeNote = { ...note, important: !note.important }
+    axios.put(url, changeNote).then((res) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : res.data)))
+    })
+    console.log('import', id)
+  }
   return (
     <div>
       <h1>Notes</h1>
@@ -47,7 +64,11 @@ const App = () => {
       </div>
       <ul>
         {noteToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
