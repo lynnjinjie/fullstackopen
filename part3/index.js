f75c26106cdbf4c638a71060e5df43127b057e91
jinjie <lynnjinjie@gmail.com>
2022-01-12 22:27:14 +0800

    exercise: 3.1-3.6

---
+++ part3/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+const persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+// get all
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+// get info
+app.get('/info', (req, res) => {
+  res.send(
+    `<div>Phonebook has info for ${persons.length} people</div> <div>${new Date()}</div>`
+  )
+})
+
+// get by id
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  person ? res.json(person) : res.status(404).end()
+})
+
+// delete by id
+app.delete('/api/persons/:id', (req, res) =>  {
+  const id = Number(req.params.id)
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = Math.ceil(Math.random(0, 1) * 100)
+  return maxId
+}
+// post
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) res.status(400).json({error:'missing name'}).end()
+  if (!body.number) res.status(400).json({error:'missing number'}).end()
+  const isHave = persons.some(person => person.number === body.number)
+  if (isHave) res.status(400).json({error: 'already have number'}).end()
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`server is listen in port: ${PORT}`)
+})
