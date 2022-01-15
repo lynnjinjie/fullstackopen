dfcca7b556a770b52fef98af8ee75657b148b498
jinjie <lynnjinjie@gmail.com>
2022-01-15 22:04:44 +0800

    exercise: 3.7-3.8

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-
+morgan.token('req', (req, res) =>{
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'))
 
 const persons = [
   { 
