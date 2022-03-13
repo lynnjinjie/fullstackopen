c8f53bf3839477febdeaab6f9eece21dea85134e
jinjie <lynnjinjie@gmail.com>
2022-03-13 21:30:43 +0800

    exercise: 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('please confirm you password!')
+  process.exit(1)
+}
+
+// 连接
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.eympu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+mongoose.connect(url)
+// schema
+const PhonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+// model
+const Phonebook = mongoose.model('Phonebook', PhonebookSchema)
+const phonebook = new Phonebook({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+// close
+phonebook.save().then(result => {
+  console.log('phonebook saved!')
+  mongoose.connection.close()
+})
