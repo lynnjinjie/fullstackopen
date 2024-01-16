1abb9b5f41940506ff851bd0894b7164581feb51
lynnjiang <lynnjinjie@gmail.com>
2024-01-16 21:10:54 +0800

    exercise: 9.1 - 9.3

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,17 @@
+const height = Number(process.argv[2])
+const weight = Number(process.argv[3])
+
+function calculateBmi(height: number, weight: number): string {
+  const BMI = weight / (height / 100) ** 2
+  if (BMI < 16) {
+    return 'Underweight'
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    return 'Normal'
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    return 'Overweight'
+  } else {
+    return 'Obese'
+  }
+}
+
+console.log(calculateBmi(height, weight))
---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,34 @@
+interface CalculateExercises {
+  periodLength: number,
+  trainingDays: number,
+  average: number,
+  target: number,
+  ratingDescription: string,
+  rating: number
+}
+
+
+function calculateExercises(time_list: number[]): CalculateExercises {
+  const periodLength = time_list.length
+  const trainingDays = time_list.filter(item => item !== 0).length
+  const average = time_list.reduce((acc, item) =>  acc += item, 0) / periodLength
+  const target = 2
+  const ratingDescription = 'not too bad but could be better'
+  const rating = 2
+  return {
+    periodLength,
+    trainingDays,
+    average,
+    target,
+    ratingDescription,
+    rating
+  }
+}
+
+const arr:number[] = []
+process.argv.slice(2).forEach((item) => {
+  arr.push(Number(item))
+})
+
+const pr = calculateExercises(arr)
+console.log('pr',pr)
